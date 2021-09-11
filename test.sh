c0955ec75652a3099323bf00c959dfb1f95baef6
Khomenko <georgia-2005@mail.ru>
2021-09-11 23:34:50 +1000

    simple update

--- test.sh
+++ test.sh
@@ -1,73 +1,73 @@
 #!/bin/sh
 
 set -e
-
+e
 buildFrontend() {
   ./backend/gradlew clean build -p backend
-  DOCKER_BUILDKIT=1 docker build -f frontend.Dockerfile frontend/ --tag frontend:v1.0-"$STUDENT_LABEL"
+  DOCKER_BUILDKIT=1 docker build -f frontend.Dockerfile frontend/ --tag frontend:v1.0-egorich88
 }
 
 buildBackend() {
   ./backend/gradlew clean build -p backend
-  DOCKER_BUILDKIT=1 docker build -f backend.Dockerfile backend/ --tag backend:v1.0-"$STUDENT_LABEL"
+  DOCKER_BUILDKIT=1 docker build -f backend.Dockerfile backend/ --tag backend:v1.0-egorich88
 }
 
 createNetworks() {
-  echo "TODO create networks"
+  echo "Создаем сеть докера"
 }
 
 createVolume() {
-  echo "TODO create volume for postgres"
+  echo "Создаем "звук" докера для Посгреса"
 }
 
 runPostgres() {
-  echo "TODO run postgres"
+  echo "Запуск докера Посгреса"
 }
 
 runBackend() {
-  echo "TODO run backend"
+  echo "Запуск докера серверной части"
 }
 
 runFrontend() {
-  echo "RUN frontend"
+  echo "Запуск интерфейса докера"
 }
 
 checkResult() {
   sleep 10
   docker exec \
-    frontend-"$STUDENT_LABEL" \
-    curl -s http://backend-"$STUDENT_LABEL":8080/api/v1/public/items > /tmp/result-"$STUDENT_LABEL"
+    frontend-egorich88 \
+    curl -s http://backend-egorich88:8080/api/v1/public/items > /tmp/result-egorich88
 
-    if [ "$(cat /tmp/result-"$STUDENT_LABEL")" != "[]" ]; then
-      echo "Check failed"
+    if [ "$(cat /tmp/result-egorich88)" != "[]" ]; then
+      echo "Не прошло проверку"
       exit 1
     fi
 }
 
 BASE_LABEL=homework1
 # TODO student surname name
-STUDENT_LABEL=
+STUDENT_LABEL=egorich88
 
-echo "=== Build backend backend:v1.0-$STUDENT_LABEL ==="
+echo "=== Создаем серверную часть backend:v1.0-egorich88 ==="
 buildBackend
 
-echo "=== Build frontend frontend:v1.0-$STUDENT_LABEL ==="
+echo "=== Создаем интерфейс frontend:v1.0-egorich88 ==="
 buildFrontend
 
-echo "=== Create networks between backend <-> postgres and backend <-> frontend ==="
+echo "=== Создать сети между серверной части <-> postgres и черверной части <-> интерфейсом ==="
 createNetworks
 
-echo "=== Create persistence volume for postgres ==="
+echo "=== Создаем постоянный том для postgres ==="
 createVolume
 
-echo "== Run Postgres ==="
+echo "== Запуск Postgres ==="
 runPostgres
 
-echo "=== Run backend backend:v1.0-$STUDENT_LABEL ==="
+echo "=== Запуск backend backend:v1.0-egorich88 ==="
 runBackend
 
-echo "=== Run frontend frontend:v1.0-$STUDENT_LABEL ==="
+echo "=== Запуск frontend frontend:v1.0-egorich88 ==="
 runFrontend
 
-echo "=== Run check ==="
+echo "=== Проверка запущена ==="
 checkResult
